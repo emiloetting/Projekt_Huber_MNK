d691c9c0fef2f777b691f46837b2ce3da44654ab
Emil Ötting <151923183+emiloetting@users.noreply.github.com>
2024-02-08 18:59:56 +0100

    Update Player.py

--- Player.py
+++ Player.py
@@ -12,33 +12,24 @@ class Player():
         Wenn ja, wird der Zug ausgeführt, andefalls muss erneute Eingabe erfolgen.
         Eingabe im Stil (Spalte, Zeile) -> (x,y)
         '''
-        coordinates_input = input(f"{self.name}, gib deine Koordinaten ein (Form: x,y): ")
-        coordinates_list = coordinates_input.split(",")                      
-
-        #prüft Gültigkeit d. Eingabe
-        if len(coordinates_input)<3:
-            print(f"{self.name}, gib 2 Werte ein!")
-            return self.make_move(board=board)
-        elif len(coordinates_input)<3:
-            print(f"{self.name}, gib 2 Werte zwischen 1 und 5 an!")
-            return self.make_move(board=board)
-        elif "," not in coordinates_input:
-            print(f"{self.name}, gib 2 Werte ein!")
-            return self.make_move(board=board)
-        
-        #Umwandlung in Array-Indexierung
-        x_coordinate = (int(coordinates_list[0]) - 1)                        
-        y_coordinate = 5 - int(coordinates_list[1])                          
-
-        if x_coordinate >= 5 or y_coordinate >= 5:                          
-            print("Ungültige Eingabe! *Keine Werte größer 5*")                                       
-            return self.make_move(board=board)                               
-        elif x_coordinate < 0 or y_coordinate < 0:                           
-            print("Ungültige Eingabe! *Keine Werte kleiner Null*")                                      
-            return self.make_move(board=board)                                  
-        elif board.return_field_value(y_coordinate, x_coordinate) != 0:      
-            print("Feld ist bereits belegt. Lege woanders")                   
-            return self.make_move(board=board)                                                          
-        else:                                                                
-            print()
-            return board.set_field_value(y_coordinate, x_coordinate, self.number)   
+        while True:
+            try:
+                coordinates_input = input(f"{self.name}, gib deine Koordinaten ein (Form: x,y): ")
+                coordinates_list = coordinates_input.split(",")  
+                #Umwandlung in Array-Indexierung
+                x_coordinate = (int(coordinates_list[0]) - 1)                        
+                y_coordinate = 5 - int(coordinates_list[1])          
+                if x_coordinate >= 5 or y_coordinate >= 5:                          
+                    print("Ungültige Eingabe! *Keine Werte größer 5*")                                       
+                    return self.make_move(board=board)                               
+                elif x_coordinate < 0 or y_coordinate < 0:                           
+                    print("Ungültige Eingabe! *Keine Werte kleiner Null*")                                      
+                    return self.make_move(board=board)                                  
+                elif board.return_field_value(y_coordinate, x_coordinate) != 0:      
+                    print("Feld ist bereits belegt. Lege woanders")                   
+                    return self.make_move(board=board)                                                          
+                else:                                                                
+                    print()
+                    return board.set_field_value(y_coordinate, x_coordinate, self.number)               
+            except Exception:
+                print(f"{self.name}, gib 2 Werte zwischen 1 und 5 ein! Trenne sie duch ein Komma!")
